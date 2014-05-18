174dcaf243f783aeb2e8fc6185a985bae73761a8
Latedi <JonathanSchramm91@gmail.com>
2014-05-18 17:38:05 +0200

    Added the btime class

---
+++ src/btime.h
@@ -0,0 +1,26 @@
+#ifndef BTIME_H
+#define BTIME_H
+
+#include <string>
+#include <cstring>
+#include <stdio.h>
+
+class BTime
+{
+private:
+	int year;
+	int month;
+	int day;
+	int hour;
+	int minute;
+public:
+	BTime();
+	BTime(int y, int mo, int d, int h, int mi);
+	void removeExcess();
+	std::string toString();
+	void insertZeros(char *buff, int len);
+	void insertChar(char *buff, char ch);
+	void addCharArrays(char *res, int var, int len, bool insertDelimiter, char delimiter);
+};
+
+#endif /* BTIME_H */
